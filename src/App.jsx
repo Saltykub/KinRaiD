ec27215cc9c167fb5b9c5a9408e84282a0475cc6
Saltykub <nathan.juir@gmail.com>
2023-12-16 14:02:33 +0700

    draft

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState ,useEffect} from "react";
+import Question from './component/Question.jsx'
+const App = () => {
+      const [count,setCount] = useState(0);
+      const [pick,setPick] = useState([]);
+      const [once,setOnce] = useState(true);
+      const [questions,setQuestions] = useState([{
+          name: 'temp1', id: 1
+        },{
+          name: 'temp2', id: 2
+        },{
+          name: 'temp3', id: 3
+        }
+      ])
+      //shuffle question everytime
+      if(once){
+        const shuffle = questions.sort(() => Math.random() - 0.5);
+        setQuestions(shuffle);
+        setOnce(false);console.log('shuffle',questions);
+      }
+      // useEffect(() => {
+      //   questions.sort(() => Math.random() - 0.5);
+      //   console.log('pass');
+      // },[])
+      console.log('after shuffle',questions);
+      if(count<questions.length){
+      return (
+        <div>
+          <h1>Choose your favorite choice</h1>
+          <Question questions={questions} pick={pick} setCount={setCount} setPick={setPick} count={count}/>
+          {console.log(pick)}
+        </div>
+      )
+      }
+      // if no more question
+      return (
+        <div>
+          <p>calculating result</p>
+          {console.log(pick)}
+        </div>
+      )
+
+}
+
+export default App;
---
+++ src/component/Question.jsx
@@ -0,0 +1,31 @@
+const Question = (props) => {
+    const {questions,pick,setPick,count,setCount}=props;
+    const handleYes = () =>{
+        const newPick = {pick:1,id:questions[count].id,name:questions[count].name};
+        const newCount = count+1;
+        setPick(pick.concat(newPick));
+        setCount(newCount);
+    }
+     const handleNo = () =>{
+        const newPick = {pick:0,id:questions[count].id,name:questions[count].name}
+        const newCount = count+1;
+        setPick(pick.concat(newPick));
+        setCount(newCount);
+    }
+     const handleMaybe = () =>{
+        const newPick = {pick:2,id:questions[count].id,name:questions[count].name};
+        const newCount = count+1;
+        setPick(pick.concat(newPick));
+        setCount(newCount);
+    }
+    return (
+        <div>
+         
+            <h1>{questions[count].name}</h1>
+            <button onClick={handleYes}>Yes</button>
+            <button onClick={handleNo}>No</button>
+            <button onClick={handleMaybe}>Maybe</button>
+        </div>
+    )
+}
+export default Question
