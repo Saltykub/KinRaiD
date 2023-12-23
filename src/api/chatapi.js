29b3cf533b00d7252184fa66d44de38bf8c1b4bb
autoastt <peeranat.kjp@gmail.com>
2023-12-23 23:17:51 +0700

    fix prompt again

--- src/api/chatapi.js
+++ src/api/chatapi.js
@@ -5,19 +5,20 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, dangerouslyAllow
 async function chatgpt(data) {
   let query = "";
   for(let i = 0; i < data.length; i++) {
+    if(data[i].pick == 'maybe') continue;
     query += data[i].name + ': ' + data[i].pick + '\n';
   }
   console.log('hi', data, query)
   const completion = await openai.chat.completions.create({
     messages: [
-        {role: "system", content: "You will be provided with food characteristics in 'yes-no-maybe' form. \
+        {role: "system", content: "You will be provided with food characteristics in 'yes-no' form. \
         Your job is to provide a list of five food corresponding to the given characteristics. \
         If the characteristic is categorized as 'yes', your food must have that trait. \
         If the characteristic is categorized as 'no', your food must not have that trait. \
-        If the characteristic is categorized as 'maybe', your food may or may not have that trait.\
+        Do note that it is not necessary to stick with the given characteristics, you can also choose some unpopular food.\
+        Your answer should not be too broad. \
         Provide your answer in bullet point (not numbering) form. \
-        Do not include any further explanation for each food: only name of food is acceptable. \
-        Your answer should not be too broad."},
+        Do not include any further explanation for each food: only name of food is acceptable."},
         {role: "user", content: query}
     ],
     model: "gpt-3.5-turbo",
